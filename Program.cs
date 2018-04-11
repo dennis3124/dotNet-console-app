d6d2f51d419b1e8b7824afc445ef776f17327e98
Dennis Chia <dennischia_1995@hotmail.com>
2018-04-11 12:26:50 -0400

    Added my project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! - Dennis Chia");
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
